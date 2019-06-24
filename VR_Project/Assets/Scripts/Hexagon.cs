13a22abc7b9a90ba6fe468d3d102a85a88d49580
Nikolas Schwarz <nikolas.schwarz@uni-konstanz.de>
2019-06-24 14:25:47 +0200

    implemented debugging view of grid

--- VR_Project/Assets/Scripts/Hexagon.cs
+++ VR_Project/Assets/Scripts/Hexagon.cs
@@ -159,7 +159,11 @@ public class Hexagon : MonoBehaviour
         throw new ArgumentOutOfRangeException("Neighbor does not exist.");
     }
 
-    public Vector3 CalcPosition()
+    /// <summary>
+    /// This calculates the position of the tile as it is defined by the Hexagon Grid. Therefore, controlGrid, colIndex and rowIndex must already be set
+    /// </summary>
+    /// <returns>Absolute position (i.e. using the coordinate system of the grid's parent)</returns>
+    public static Vector3 CalcPosition(HexagonGrid controlGrid, int rowIndex, int colIndex)
     {
         //x-axis corresponds to col with the same orientation, z-axis to row but with inverted orientation
         float rightOffset = 0.0f;
@@ -171,6 +175,9 @@ public class Hexagon : MonoBehaviour
         return controlGrid.Position + new Vector3(colIndex * controlGrid.TileDiam + rightOffset, 0, -rowIndex * zDistance);
     }
 
+    /// <summary>
+    /// The orientation of the hexagon, where Right is the default
+    /// </summary>
     public Direction Orientation
     {
         get
@@ -182,11 +189,14 @@ public class Hexagon : MonoBehaviour
         {
             orientation = value;
             Vector3 oldRot = gameObject.transform.rotation.eulerAngles;
-            oldRot.z = -(int)orientation * 60.0f;
+            oldRot.y = -(int)orientation * 60.0f;
             gameObject.transform.rotation = Quaternion.Euler(oldRot);
         }
     }
 
+    /// <summary>
+    /// Unique binary encoding independent of orientation based on which of the six boundaries have an exit
+    /// </summary>
     public byte Encoding
     {
         get
@@ -201,6 +211,25 @@ public class Hexagon : MonoBehaviour
 
     public Direction EncodeStart => encodeStart;
 
+    public bool HasExitGlobal(Direction d)
+    {
+        return hasExit[(d - orientation + 6) % 6];
+    }
+
+    public bool isConnected(Direction d)
+    {
+        if(this.GetNeighbor(d) == null)
+        {
+            return false;
+        }
+        return this.HasExitGlobal(d) && this.GetNeighbor(d).HasExitGlobal((Direction)(((int)d + 3) % 6));
+    }
+
+    /// <summary>
+    /// Creates a Hexagon which has exits at the specified directions. 
+    /// </summary>
+    /// <param name="exits">directions with an exit</param>
+    /// <returns>Reference to the just created Hexagon</returns>
     public static Hexagon Create(params Hexagon.Direction[] exits)
     {
         // calculate encoding:
@@ -211,26 +240,101 @@ public class Hexagon : MonoBehaviour
             hasExit[(int)exit] = true;
         }
 
-        (byte encoding, Hexagon.Direction dir) = Hexagon.ExitArrayToEncoding(hasExit);
-
-        List<Hexagon> hexagonOfType;
-        bool success = hexPrefabs.TryGetValue(encoding, out hexagonOfType);
-        //Debug.Log("Success: " + success);
-        hexPrefabsUsageIndex.TryGetValue(encoding, out int offset);
-        if(offset / hexagonOfType.Count >= MAX_DUPLICATE_TILES)
+        if (!HexagonGrid.debugMode)
         {
-            throw new Exception("Maximum amount of instances from the same prefab has been exceeded");
+            (byte encoding, Hexagon.Direction dir) = Hexagon.ExitArrayToEncoding(hasExit);
+
+            List<Hexagon> hexagonOfType;
+            bool success = hexPrefabs.TryGetValue(encoding, out hexagonOfType);
+            //Debug.Log("Success: " + success);
+            hexPrefabsUsageIndex.TryGetValue(encoding, out int offset);
+            if (offset / hexagonOfType.Count >= MAX_DUPLICATE_TILES)
+            {
+                throw new Exception("Maximum amount of instances from the same prefab has been exceeded");
+            }
+            Hexagon prefab = hexagonOfType[offset % hexagonOfType.Count];
+            hexPrefabsUsageIndex.Remove(encoding);
+            hexPrefabsUsageIndex.Add(encoding, offset + 1);
+
+            Hexagon.Direction relDir = (Hexagon.Direction)((dir - prefab.EncodeStart + 6) % 6);
+            Debug.Log("dir: " + dir + " EncodeStart: " + prefab.EncodeStart + " RelDir: " + relDir);
+
+            return Hexagon.Create(prefab.gameObject, relDir);
         }
-        Hexagon prefab = hexagonOfType[offset % hexagonOfType.Count];
-        hexPrefabsUsageIndex.Remove(encoding);
-        hexPrefabsUsageIndex.Add(encoding, offset + 1);
+        else
+        {
+            (byte encoding, Hexagon.Direction dir) = Hexagon.ExitArrayToEncoding(hasExit);
 
-        Hexagon.Direction relDir = (Hexagon.Direction)((dir - prefab.EncodeStart + 6) % 6);
-        Debug.Log("dir: " + dir + " EncodeStart: " + prefab.EncodeStart + " RelDir: " + relDir);
+            hexPrefabsUsageIndex.TryGetValue(encoding, out int offset);
+            /*if (offset / hexagonOfType.Count >= MAX_DUPLICATE_TILES)
+            {
+                Debug.LogWarning("Maximum amount of instances from the same prefab has been exceeded");
+            }*/
+            hexPrefabsUsageIndex.Remove(encoding);
+            hexPrefabsUsageIndex.Add(encoding, offset + 1);
 
-        return Hexagon.Create(prefab.gameObject, relDir);
+
+            List<Hexagon> hexagonOfType;
+            bool success = hexPrefabs.TryGetValue(0, out hexagonOfType);
+            Hexagon prefab = hexagonOfType[0];
+            Hexagon.Direction relDir = (Hexagon.Direction)((dir - prefab.EncodeStart + 6) % 6);
+
+            Hexagon dummy = Hexagon.Create(prefab.gameObject, relDir);
+
+            dummy.hasExit = hasExit;
+            //TODO: draw lines
+            float xDistance = 12.0f/2.0f;
+            float zDistance = Mathf.Sqrt(0.75f * Mathf.Pow(xDistance, 2));
+
+            for (int i = 0; i < 6; i++)
+            {
+                if(dummy.hasExit[i])
+                {
+                    Vector3 edgePoint = new Vector3();
+                    switch ((Direction) i)
+                    {
+                        case Direction.Right:
+                            edgePoint.x = xDistance;
+                            break;
+                        case Direction.Left:
+                            edgePoint.x = -xDistance;
+                            break;
+
+                        case Direction.TopRight:
+                            edgePoint.x = xDistance/2.0f;
+                            edgePoint.z = zDistance;
+                            break;
+                        case Direction.BotLeft:
+                            edgePoint.x = -xDistance / 2.0f;
+                            edgePoint.z = -zDistance;
+                            break;
+
+                        case Direction.TopLeft:
+                            edgePoint.x = -xDistance / 2.0f;
+                            edgePoint.z = zDistance;
+                            break;
+
+                        case Direction.BotRight:
+                            edgePoint.x = xDistance / 2.0f;
+                            edgePoint.z = -zDistance;
+                            break;
+                    }
+
+                    LineRenderer linRen = (new GameObject("edge")).AddComponent<LineRenderer>();
+                    linRen.useWorldSpace = false;
+                    linRen.positionCount = 2;
+                    linRen.SetPosition(0, new Vector3(0.0f,0.0f,0.0f));
+                    linRen.SetPosition(1, edgePoint);
+                    linRen.widthMultiplier = 0.1f;
+                    linRen.gameObject.transform.parent = dummy.gameObject.transform;
+                }
+            }
+            //dummy.Orientation = (Direction)UnityEngine.Random.Range(0, 6);
+            return dummy;
+        }
     }
-        public static Hexagon Create(GameObject prefabHexagon, Direction orientation)
+
+    public static Hexagon Create(GameObject prefabHexagon, Direction orientation)
     {
         if (prefabHexagon.GetComponent<Hexagon>() == null)
         {
@@ -241,13 +345,11 @@ public class Hexagon : MonoBehaviour
         GameObject hObj = Instantiate(prefabHexagon, new Vector3(0, 0, 0), Quaternion.identity);
         Hexagon h = hObj.GetComponent<Hexagon>();
         // The Prefab rotation is dropped so we need to set it again
-        h.gameObject.transform.rotation = Quaternion.Euler(-90, 0, 0);
+        //h.gameObject.transform.rotation = Quaternion.Euler(-90, 0, 0);
         h.Orientation = orientation;
 
         return h;
     }
-
-
 
     public static (byte, Direction) ExitArrayToEncoding(bool[] hasExit)
     {
--- VR_Project/Assets/Scripts/HexagonGrid.cs
+++ VR_Project/Assets/Scripts/HexagonGrid.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class HexagonGrid : MonoBehaviour
 {
-    private int gridWidth = 10; // amount of tiles in x-direction
-    private int gridHeight = 10; // amount of tiles in y-direction
+    private int gridWidth = 7; // amount of tiles in x-direction
+    private int gridHeight = 7; // amount of tiles in y-direction
     private Vector3 position; // position of the first tile's center
     private float tileDiam = 12.0f; // distance from an edge to the opposite edge by default
+    public static readonly bool debugMode = true;
     public readonly bool vertexDistance = true; // use distance from vertex to vertex for tileDiam
     private Hexagon[,] hexagons;
    
@@ -55,20 +56,59 @@ public class HexagonGrid : MonoBehaviour
 
         hexagons = new Hexagon[gridHeight, gridWidth];
 
-        Hexagon.Direction[] leftRight = { Hexagon.Direction.Right, Hexagon.Direction.Left };
-        Hexagon.Direction[] tleftBright = { Hexagon.Direction.BotRight, Hexagon.Direction.TopLeft };
-        Hexagon.Direction[] empty = { };
+        Hexagon.Direction[] deadEnd = { Hexagon.Direction.Right };
 
-        Hexagon.Direction[,][] griddirs = { { leftRight, leftRight, leftRight, leftRight, leftRight, leftRight, leftRight },
-            { leftRight, leftRight, leftRight, leftRight, leftRight, leftRight, leftRight },
-            { leftRight, empty, tleftBright, leftRight, leftRight, leftRight, leftRight },
-            { leftRight, leftRight, leftRight, leftRight, leftRight, leftRight, leftRight },
-            { leftRight, leftRight, leftRight, leftRight, leftRight, leftRight, leftRight },
-            { leftRight, leftRight, leftRight, leftRight, leftRight, leftRight, leftRight },
-            { leftRight, leftRight, leftRight, leftRight, leftRight, leftRight, leftRight },
+        Hexagon.Direction[] straight = { Hexagon.Direction.Right, Hexagon.Direction.Left };
+        Hexagon.Direction[] wideCurve = { Hexagon.Direction.Right, Hexagon.Direction.TopLeft};
+        Hexagon.Direction[] tightCurve = { Hexagon.Direction.Right, Hexagon.Direction.TopRight };
+
+        Hexagon.Direction[] yJct = { Hexagon.Direction.Right, Hexagon.Direction.TopLeft, Hexagon.Direction.BotLeft };
+        Hexagon.Direction[] leftSplitoff = { Hexagon.Direction.Right, Hexagon.Direction.Left, Hexagon.Direction.BotLeft };
+        Hexagon.Direction[] rightSplitoff = { Hexagon.Direction.Right, Hexagon.Direction.Left, Hexagon.Direction.TopLeft };
+        Hexagon.Direction[] arrowJct = { Hexagon.Direction.Right, Hexagon.Direction.TopRight, Hexagon.Direction.TopLeft };
+
+        Hexagon.Direction[] xJct = { Hexagon.Direction.Right, Hexagon.Direction.Left, Hexagon.Direction.TopRight, Hexagon.Direction.BotLeft };
+        Hexagon.Direction[] trident = { Hexagon.Direction.Right, Hexagon.Direction.Left, Hexagon.Direction.TopRight, Hexagon.Direction.BotRight };
+        Hexagon.Direction[] crippleFourwayJct = { Hexagon.Direction.Left, Hexagon.Direction.TopLeft, Hexagon.Direction.TopRight, Hexagon.Direction.Right };
+
+        Hexagon.Direction[] fivewayJct = { Hexagon.Direction.Left, Hexagon.Direction.TopLeft, Hexagon.Direction.TopRight, Hexagon.Direction.BotLeft, Hexagon.Direction.BotRight };
+
+        Hexagon.Direction[] allwayJct = { Hexagon.Direction.Right, Hexagon.Direction.Left, Hexagon.Direction.TopLeft, Hexagon.Direction.TopRight, Hexagon.Direction.BotLeft, Hexagon.Direction.BotRight };
+
+
+        List<Hexagon.Direction[]> tiles = new List<Hexagon.Direction[]>
+        {
+            deadEnd, deadEnd, deadEnd, deadEnd, deadEnd, deadEnd,
+            straight, straight, straight, straight, wideCurve, wideCurve, wideCurve, wideCurve, tightCurve, tightCurve, tightCurve, tightCurve,
+            yJct, yJct, yJct, yJct, leftSplitoff, leftSplitoff, leftSplitoff, leftSplitoff, rightSplitoff, rightSplitoff, rightSplitoff, rightSplitoff, arrowJct, arrowJct, arrowJct, arrowJct,
+            xJct, xJct, xJct, xJct, trident, trident, trident, trident, crippleFourwayJct, crippleFourwayJct, crippleFourwayJct, crippleFourwayJct,
+            fivewayJct, fivewayJct,
+            allwayJct, allwayJct
         };
+
+        List<Hexagon.Direction[]> tilesShuffled = new List<Hexagon.Direction[]>();
+
         
-       CreateHexagons(griddirs);
+
+        for(int listLength = tiles.Count; listLength> 0; listLength--)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, listLength);
+            tilesShuffled.Add(tiles[randomIndex]);
+            tiles.RemoveAt(randomIndex);
+        }
+        Debug.Log("tiles count: " + tilesShuffled.Count);
+        Hexagon.Direction[,][] griddirs = new Hexagon.Direction[gridHeight,gridWidth][];
+        for (int row = 0; row < gridHeight; row++)
+        {
+            for (int col = 0; col < gridWidth; col++)
+            {
+                Debug.Log("Index: (" + row + "," + col + ")");
+                griddirs[row, col] = tilesShuffled[0];
+                tilesShuffled.RemoveAt(0);
+            }
+        }
+
+        CreateHexagons(griddirs);
 
         /*
         for(int row = 0; row < gridHeight; row++)
@@ -126,7 +166,7 @@ public class HexagonGrid : MonoBehaviour
         h.controlGrid = this;
         h.rowIndex = row;
         h.colIndex = col;
-        h.gameObject.transform.position = h.CalcPosition();
+        h.gameObject.transform.position = Hexagon.CalcPosition(this, row, col);
 
         if (vertexDistance)
         {
