df0152c63d326b59b9698721bc2ebf292713d4a5
philip <moepforfreedom128@gmail.com>
2019-07-16 00:34:44 +0200

    addeed some functionality to the locked door

---
+++ VR_Project/Assets/Scripts/DoorLocked.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLocked : MonoBehaviour
+{
+  private Animator anim;
+  public GameObject[] symbols;
+  public int[] targetCode;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    anim = GetComponent<Animator>();
+    //anim.Play("AN_Door_open", 0, 0);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
