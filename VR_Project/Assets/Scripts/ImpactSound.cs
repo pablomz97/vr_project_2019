147e8dc8c4c9492d8bbded8b1d90c718b7ae3c8c
Pablo Martinez <pablomzblasco@gmail.com>
2019-07-19 07:47:36 +0200

    added sounds and last tpArea

---
+++ VR_Project/Assets/Scripts/ImpactSound.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip[] sound;
+    [Range(0f,1f)]
+    public float volume;
+    System.Random rnd = new System.Random();
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        source.loop = false;
+        source.rolloffMode = AudioRolloffMode.Logarithmic;
+        source.minDistance = 1;
+        source.minDistance = 6;
+        source.volume = volume;
+        source.playOnAwake = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        int i = rnd.Next(0, sound.Length);
+        source.clip = sound[i];
+        source.Play();
+    }
+}
